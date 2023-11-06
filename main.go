42c27f0bc363e43fa5e86d3f9c23f82a00990e7c
Yashwanthreddyk2199 <yashwanth.k2199@gmail.com>
2023-11-06 16:49:43 -0500

    custom packages

---
+++ main.go
@@ -0,0 +1,22 @@
+package main // always starts with
+
+import (
+	"fmt" // contains all the basic functions
+	//"myproject/simplecalc" // error in linking shows not in the src directory
+)
+
+func main() {
+	//a, b := 12, 3 // variable declaratrion
+	//fmt.Println(simplecalc.Add(a, b))
+	//fmt.Println(simplecalc.Sub(a, b))
+	//fmt.Println(simplecalc.Mul(a, b))
+	
+	
+	fmt.Println("Hello") // function always start with capital EX: Println, Add
+
+	for i:=1; i<10; i++{    //for loop
+
+		fmt.Println("Hello")
+
+	}
+}
---
+++ simplecalc/calc.go
@@ -0,0 +1,11 @@
+import simplecalc // to specify to which it is linked
+
+func Add(a,b int)int{  // function starts with capital and the output type should be declared before 
+	return a+b
+}
+func Sub(a,b int)int{
+	return a-b
+}
+func Mul1(a,b int)int{
+	return a*b
+}
